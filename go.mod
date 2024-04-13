19a0037dd03440d12d58cc188c4895a31a96d074
Mike Fridman <mf192@icloud.com>
2024-04-13 14:36:23 -0400

    Use mfridman/buildversion

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.22.1
 
 require (
 	github.com/bufbuild/protoplugin v0.0.0-20240323223605-e2735f6c31ee
+	github.com/mfridman/buildversion v0.1.1
 	google.golang.org/protobuf v1.33.0
 )
--- main.go
+++ main.go
@@ -6,12 +6,10 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
-	"runtime/debug"
-	"strings"
 	"syscall"
-	"time"
 
 	"github.com/bufbuild/protoplugin"
+	"github.com/mfridman/buildversion"
 	"github.com/mfridman/protoc-gen-connectclient-go/internal/plugin"
 )
 
@@ -24,7 +22,7 @@ func main() {
 	defer stop()
 
 	environ := os.Environ()
-	environ = append(environ, plugin.PLUGIN_VERSION+"="+getVersionFromBuildInfo())
+	environ = append(environ, plugin.PLUGIN_VERSION+"="+buildversion.New(version))
 
 	go func() {
 		defer stop()
@@ -62,7 +60,7 @@ func runArgs(args []string) error {
 	for _, arg := range args {
 		switch arg {
 		case "--version", "-version":
-			fmt.Fprintf(os.Stdout, "%s version: %s\n", plugin.Name, strings.TrimSpace(getVersionFromBuildInfo()))
+			fmt.Fprintf(os.Stdout, "%s version: %s\n", plugin.Name, buildversion.New(version))
 			os.Exit(0)
 		default:
 			fmt.Fprintf(os.Stderr, "%s: unknown argument: %s\n", plugin.Name, arg)
@@ -70,66 +68,4 @@ func runArgs(args []string) error {
 		}
 	}
 	return nil
-}
-
-// getVersionFromBuildInfo returns the version string from the build info, if available. It will
-// always return a non-empty string.
-//
-//   - If the build info is not available, it returns "devel".
-//   - If the main version is set, it returns the string as is.
-//   - If building from source, it returns "devel" followed by the first 12 characters of the VCS
-//     revision, followed by ", dirty" if the working directory was dirty. For example,
-//     "devel (abcdef012345, dirty)" or "devel (abcdef012345)". If the VCS revision is not available,
-//     "unknown revision" is used instead.
-//
-// Note, vcs info not stamped when built listing .go files directly. E.g.,
-//   - `go build main.go`
-//   - `go build .`
-//
-// For more information, see https://github.com/golang/go/issues/51279
-func getVersionFromBuildInfo() string {
-	if version != "" {
-		return version
-	}
-	const defaultVersion = "devel"
-
-	buildInfo, ok := debug.ReadBuildInfo()
-	if !ok {
-		// Should only happen if -buildvcs=false is set or using a really old version of Go.
-		return defaultVersion
-	}
-	// The (devel) string is not documented, but it is the value used by the Go toolchain. See
-	// https://github.com/golang/go/issues/29228
-	if s := buildInfo.Main.Version; s != "" && s != "(devel)" {
-		return buildInfo.Main.Version
-	}
-	var vcs struct {
-		revision string
-		time     time.Time
-		modified bool
-	}
-	for _, setting := range buildInfo.Settings {
-		switch setting.Key {
-		case "vcs.revision":
-			vcs.revision = setting.Value
-		case "vcs.time":
-			vcs.time, _ = time.Parse(time.RFC3339, setting.Value)
-		case "vcs.modified":
-			vcs.modified = (setting.Value == "true")
-		}
-	}
-
-	var b strings.Builder
-	b.WriteString(defaultVersion)
-	b.WriteString(" (")
-	if vcs.revision == "" || len(vcs.revision) < 12 {
-		b.WriteString("unknown revision")
-	} else {
-		b.WriteString(vcs.revision[:12])
-	}
-	if vcs.modified {
-		b.WriteString(", dirty")
-	}
-	b.WriteString(")")
-	return b.String()
 }
